3372232bb8da89afdbad79c7a29b52138d8f3dc8
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-09-08 17:20:44 +0200

    added BasicAutomation class and selenium dependencies

---
+++ CodiceFiscale/src/main/java/AutoMATion/CodiceFiscale/BasicAutomation.java
@@ -0,0 +1,56 @@
+package AutoMATion.CodiceFiscale;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasicAutomation {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		driver.get("https://www.codicefiscaleonline.com/");
+
+		// accetta i consensi
+		driver.findElement(By.className("fc-cta-consent")).click();
+
+		driver.findElement(By.id("input_cognome")).sendKeys("Cavalieri Manasse");
+		driver.findElement(By.id("input_nome")).sendKeys("Mattia");
+		driver.findElement(By.name("luogo")).sendKeys("Milano");
+		driver.findElement(By.name("prov")).sendKeys("Mi");
+
+		// seleziona il giorno di nascita
+		WebElement giornoNascita = driver
+				.findElement(By.cssSelector("#calcolo > div.field.data > div.input > select:nth-child(1)"));
+		Select giorno = new Select(giornoNascita);
+		giorno.selectByVisibleText("06");
+
+		// seleziona il mese di nascita
+		WebElement meseNascita = driver
+				.findElement(By.cssSelector("#calcolo > div.field.data > div.input > select:nth-child(2)"));
+		Select mese = new Select(meseNascita);
+		mese.selectByVisibleText("12");
+
+		// seleziona l'anno di nascita
+		WebElement annoNascita = driver
+				.findElement(By.cssSelector("#calcolo > div.field.data > div.input > select:nth-child(3)"));
+		Select anno = new Select(annoNascita);
+		anno.selectByVisibleText("1980");
+
+		driver.findElement(By.cssSelector("#calcolo > div.submit > input[type=submit]")).click();
+
+		wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#calcolo > div.field.cf > div.input")));
+		String CF = driver.findElement(By.cssSelector("#calcolo > div.field.cf > div.input")).getText();
+		System.out.println("Il codice fiscale generato è: " + CF);
+	}
+
+}
